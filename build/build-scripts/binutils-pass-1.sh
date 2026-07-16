7d48b6d22298463aaa4c361bce93c70b54997061
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-07-16 16:17:49 +0800

    created build script template and binutils-pass-1 script

---
+++ build/build-scripts/binutils-pass-1.sh
@@ -0,0 +1,21 @@
+cd $LFS/sources
+tar -xf binutils-2.46.0.tar.xz
+cd binutils-2.46.0
+# /////////////////// #
+mkdir -v build
+cd       build
+
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu
+
+make
+make install
+# /////////////////// #
+cd $LFS/sources
+rm -rf binutils-2.46.0
---
+++ build/build-scripts/template.sh
@@ -0,0 +1,8 @@
+cd $LFS/sources
+tar -xf [PACKAGE_NAME].tar.xz
+cd [PACKAGE_NAME]
+# /////////////////// #
+# Build specific code #
+# /////////////////// #
+cd $LFS/sources
+rm -rf [PACKAGE_NAME]
