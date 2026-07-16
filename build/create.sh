f5344eef4392eae44d3979ab05ec4fae58f29c83
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-07-16 16:36:25 +0800

    Update create.sh

--- build/create.sh
+++ build/create.sh
@@ -5,3 +5,9 @@ if ! test $(whoami) == "lfs" ; then
     echo "Must be run as lfs user!"
     exit -l
 fi
+
+echo "Creating Build Environment"
+cd $DIST_ROOT/build
+
+bash -e build-scripts/binutils-pass-1.sh
+bash -e build-scripts/gcc-pass-1.sh
