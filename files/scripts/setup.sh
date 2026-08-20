4e4c1008e1121f880b633407594c3493aeb19065
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-08-20 19:17:26 +0800

    attempt to set default theming on login screen

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -15,3 +15,23 @@ sudo sed -i "s|^SHELL=.*|SHELL=/usr/bin/zsh|" /etc/default/useradd
 rm /etc/skel/.bash_logout
 rm /etc/skel/.bash_profile
 rm /etc/skel/.bashrc
+
+# theming lock screen
+setfacl -m u:greeter:x /etc/skel/
+setfacl -m u:greeter:x /etc/skel/.config
+setfacl -m u:greeter:x /etc/skel/.local
+setfacl -m u:greeter:x /etc/skel/.local/state
+setfacl -m u:greeter:x /etc/skel/.local/share
+
+sudo chgrp -R greeter /etc/skel/.config/DankMaterialShell
+sudo chmod -R g+rX /etc/skel/.config/DankMaterialShell
+
+sudo chgrp -R greeter /etc/skel/.local/state/DankMaterialShell
+sudo chmod -R g+rX /etc/skel/.local/state/DankMaterialShell
+
+sudo chgrp -R greeter /etc/skel/.local/share/color-schemes/
+sudo chmod -R g+rX /etc/skel/.local/share/color-schemes/
+
+sudo ln -sf /etc/skel/.config/DankMaterialShell/settings.json /var/cache/dms-greeter/settings.json
+sudo ln -sf /etc/skel/.local/state/DankMaterialShell/session.json /var/cache/dms-greeter/session.json
+sudo ln -sf /etc/skel/.local/share/color-schemes/dms-colors.json /var/cache/dms-greeter/colors.json
